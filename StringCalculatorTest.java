720443e2ad11bb969fca36c0f8beb5f5f361f663
Vaishnavi1812 <56027284+Vaishnavi1812@users.noreply.github.com>
2022-09-07 23:22:34 +0530

    Test3
    --> Testing for an unknown amount of numbers
    --> Also added assertions to validate answers returning from the function

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,31 +1,33 @@
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	void test() {
+//		StringCalculator.add("0");
+//		Assertions.assertTrue(true);
+//	}
 
 	@Test
 	public void SampleTestCasePassingNull() {
-		StringCalculator.add("");
-		Assertions.assertTrue(true);
+		Assertions.assertEquals(0, StringCalculator.add(""));
 	}
 
 	@Test
 	public void SampleTestCasePassingSingleNumberInString() {
-		StringCalculator.add("1");
-		Assertions.assertTrue(true);
+		Assertions.assertEquals(1, StringCalculator.add("1"));
 	}
 
 	@Test
 	public void SampleTestCasePassingTwoNumbersInString() {
-		StringCalculator.add("1,2");
-		Assertions.assertTrue(true);
+		Assertions.assertEquals(3, StringCalculator.add("1,2"));
+	}
+
+	@Test
+	public void SampleTestCasePassingUnknownAmountOfNumbersInString() {
+		Assertions.assertEquals(220, StringCalculator
+				.add("1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10"));
 	}
 
 }
