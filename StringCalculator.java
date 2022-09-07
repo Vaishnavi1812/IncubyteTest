33eb8be11e5aa3905579bda7b9f2729c9ecf5000
Vaishnavi1812 <56027284+Vaishnavi1812@users.noreply.github.com>
2022-09-07 22:57:06 +0530

    Test2
    -->for testcases: "", "1," "1,2" 
    -->The third testcase is failing

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
 
 public class StringCalculator {
 
-	public void add(String numbers) {
-
+	public static int add(String numbers) {
+		ArrayList<Integer> numbersFromString = new ArrayList<Integer>();
+		String num = "";
+		for (int i = 0; i < numbers.length(); i++) {
+			num = "";
+			if (numbers.charAt(i) != ',') {
+				num += numbers.charAt(i);
+			} else {
+				Integer integerNum = Integer.parseInt(num);
+				numbersFromString.add(integerNum);
+				num = "";
+			}
+		}
+		if (num != "") {
+			Integer integerNum = Integer.parseInt(num);
+			numbersFromString.add(integerNum);
+		}
+		int sum = 0;
+		for (int i = 0; i < numbersFromString.size(); i++) {
+			sum += numbersFromString.get(i);
+		}
+		return sum;
 	}
 }
--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,5 +1,6 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
@@ -7,6 +8,24 @@ class StringCalculatorTest {
 	@Test
 	void test() {
 		fail("Not yet implemented");
+	}
+
+	@Test
+	public void SampleTestCasePassingNull() {
+		StringCalculator.add("");
+		Assertions.assertTrue(true);
+	}
+
+	@Test
+	public void SampleTestCasePassingSingleNumberInString() {
+		StringCalculator.add("1");
+		Assertions.assertTrue(true);
+	}
+
+	@Test
+	public void SampleTestCasePassingTwoNumbersInString() {
+		StringCalculator.add("1,2");
+		Assertions.assertTrue(true);
 	}
 
 }
